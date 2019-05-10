73999bdd98bdcd8125730250508f6aa8d75aa75c
LeoGarridoR <leonardo,garridoreyes@gmail.com>
2019-05-09 22:50:33 -0400

    Actualiza el script para usar funciones, e incluye el modulo CLICK, y muestra informacion detallada de cada instancia, incorpora detener instancias, y filtrar por tag de proyectos

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,18 +4,55 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project','Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Comandos para las instancias"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+     help="Sólo instancias para el proyecto (tag Project:<name>)")
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
             i.public_dns_name,
-            str(i.ami_launch_index))))
+            str(i.ami_launch_index),
+            tags.get('Project', '<no project>')
+            )))
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None,
+   help='Solamente instacias de proyectos')
+def stop_instances(project):
+    "Detener instancias EC2"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Deteniendo {0}....".format(i.id))
+        i.stop()
+
     return
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
